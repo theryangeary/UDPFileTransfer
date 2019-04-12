8f330b93e738265f1b4f67d3c4bc3f8fa4121978
Ryan Geary <rtgnj42@gmail.com>
2019-04-12 17:28:26 -0400

    Rename fileBuffer to rcvBuffer

--- client.c
+++ client.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
   unsigned short serverPort;
   char* serverIP;
   char* filename;
-  char fileBuffer[RECV_BUF_SIZE];
+  char rcvBuffer[RECV_BUF_SIZE];
   unsigned int filenameLength;
   int bytesReceived, totalBytesReceived;
 
