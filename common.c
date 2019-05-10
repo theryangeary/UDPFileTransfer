f6f4473e4bddb16768d4d9c48fa1fc228fe433fc
Ryan Geary <rtgnj42@gmail.com>
2019-05-10 01:02:14 -0400

    Add stack trace printing for debugging

--- common.c
+++ common.c
@@ -13,5 +13,17 @@ unsigned char checksum(char* fileByte, size_t size, unsigned char check) {
   while (0 != size--) {
     check -= *fileByte++;
   }
+  /*printCallingExecutable();*/
+  /*printf(": Checksum value: %02x\n", check);*/
   return check;
+}
+
+void printCallingExecutable() {
+  int nptrs;
+  void *buffer[SIZE];
+  char **strings;
+  nptrs = backtrace(buffer, SIZE);
+  strings = backtrace_symbols(buffer, nptrs);
+  printf("%s", strings[0]);
+  free(strings);
 }
--- common.h
+++ common.h
@@ -5,10 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <execinfo.h>
 
 #define MAXSOCK 5
 #define RECV_BUF_SIZE 32
+#define SIZE 100
 
 void throwError(char* msg);
 void throwErrorWithCode(char* msg, int exitCode);
 unsigned char checksum(char* fileByte, size_t size, unsigned char check);
+void printCallingExecutable();
